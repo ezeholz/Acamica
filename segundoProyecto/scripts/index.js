4d1585921e42e7cf826c175ade785391691d0cff
Ezequiel Holzweissig <ezequielgh1999@gmail.com>
2020-01-21 13:47:06 -0300

    muchos cambios
    crear eliminado, por similitud con misgifos
    esperemos que este sea el ultimo commit referido a index, por ahora quedaría armar la ultima pestaña y agregar la libreria
    por hoy me quedo satisfecho

--- segundoProyecto/scripts/index.js
+++ segundoProyecto/scripts/index.js
@@ -3,13 +3,21 @@
 window.onclick = function(event) {
     if (!event.target.matches('.drop')) {
         if (document.getElementById("menuTemas").classList.contains("mostrar")) {
-            this.abrirTemas();
+            this.abrirMenu("menuTemas");
         }
     }
 }
 
-function abrirTemas() {
-    document.getElementById("menuTemas").classList.toggle("mostrar");
+window.onkeypress = function(event) {
+    if(event.keyCode == 13 && event.target.matches('#barBuscar')) {
+        this.search(document.getElementById("barBuscar").value)
+    }
+}
+
+if(localStorage.getItem('tema')=="oscuro"){cambiarTema()}
+
+function abrirMenu(id,force) {
+    document.getElementById(id).classList.toggle("mostrar",force);
 }
 
 function cambiarTema() {
@@ -19,41 +27,68 @@ function cambiarTema() {
     }
     document.getElementById("menuTemas").getElementsByTagName("button")[0].toggleAttribute("disabled");
     document.getElementById("menuTemas").getElementsByTagName("button")[1].toggleAttribute("disabled");
+    if(lista[0].classList.value.includes('oscuro')){localStorage.setItem('tema','oscuro')}
+    else{localStorage.setItem('tema','claro')}
 }
-
-// Sugerencias
 
 function ratio(w,h) {
     //ratio = Math.ceil(Math.ceil(json.data[i].images.downsized.width / json.data[i].images.downsized.height)-Math.ceil(json.data[i].images.downsized.height / json.data[i].images.downsized.width));
     return Math.ceil(Math.ceil(w / h)-Math.ceil(h / w))
 }
 
-var sugerencias = [
-    "gato","perro","animales","programming","white guy blinking",/*"john travolta",*/"garfield","rick and morty","los simpsons","internet","html","css","beproud","lgbtq+",
-]
+// Sugerencias
 
-while(sugerencias.length!=4) {
-    number = Math.floor(Math.random() * sugerencias.length);
-    sugerencias.splice(number, 1);
+
+
+function fetchSug() {
+    if(localStorage.getItem('lastSearchs') == null) {localStorage.setItem('lastSearchs',
+        "gato;perro;animales;programming;white guy blinking;"/*"john travolta";*/+"billie eilish;rick and morty;los simpsons;internet;html;css;beproud;lgbtq+;"
+    )}
+    var sugerencias = localStorage.getItem('lastSearchs').split(';')
+    sugerencias.splice(sugerencias.length-1,1)
+    return sugerencias
 }
 
+var sugerencias = fetchSug()
 
+if(sugerencias.length < 4) {localStorage.setItem('lastSearchs',
+    sugerencias+"gato;perro;animales;programming;white guy blinking;"/*"john travolta";*/+"garfield;rick and morty;los simpsons;internet;html;css;beproud;lgbtq+;"
+);sugerencias=fetchSug()}
+
+async function sugeridos() {
+    var sugerAux = sugerencias
+    sugerAux.splice(sugerAux.length-1,1)
+    for(i=sugerAux.length;i!=4;i=sugerAux.length) {
+        number = Math.floor(Math.random() * i);
+        sugerAux.splice(number, 1);
+    }
+    index(sugerAux)
+}
 
 fetch("https://worried-passive.glitch.me/counter")
-    .then(response => {return response.json()})
-    .then(json => {
+    .then(function (response) {return response.json()})
+    .then(function (json) {
         var texto = "¡Bienvenidos/as a Guifos.com! ——————Donde los gifs están.////// Número de visitas: " + json.visit
         document.getElementsByClassName("barraSuperior")[0].childNodes[1].innerText = texto
     })
 
-async function index() {
+async function index(sugerAux) {
 
     for(i=1;i<=4;i++) {
         //console.log(i);
-        document.getElementById("sug" + i).innerText = sugerencias[i - 1];
-        await fetch("https://api.giphy.com/v1/gifs/search?api_key=HAH9qg4gGd6m3JwsSJUWkAL6mvkcEVBp&limit=25&q="+sugerencias[i - 1])
-            .then(response => {return response.json();})
-            .then(json => {
+        document.getElementById("sug" + i).innerHTML = sugerAux[i - 1] + '<img src="./images/button%20close.svg" alt="button close">';
+        document.getElementById("sug" + i).addEventListener("click", function (event) {if(event.target.alt == "button close"){
+            var a1 = localStorage.getItem('lastSearchs')
+            var a2 = a1.split(document.getElementById(this.id).innerText+';')[0]+a1.split(document.getElementById(this.id).innerText+';')[1]
+            //console.log(a1)
+            //console.log(a2)
+            localStorage.setItem('lastSearchs',a2)
+            //console.log(localStorage.getItem('lastSearchs'))
+            location.reload()
+        }})
+        await fetch("https://api.giphy.com/v1/gifs/search?api_key=HAH9qg4gGd6m3JwsSJUWkAL6mvkcEVBp&limit=25&q="+sugerAux[i - 1])
+            .then(function (response) {return response.json();})
+            .then(function (json) {
                 //console.log(json);
                 for(x=0;x<json.data.length;x++){
                     if(ratio(json.data[x].images.downsized.width,json.data[x].images.downsized.height) == 0){
@@ -71,19 +106,21 @@ async function index() {
 }
 
 // Tendencias
+
 async function trend() {
 
-    total = Math.round(((window.screen.height*1.2)/330)*3);
+    total = Math.round(((window.screen.height*1.2)/330)*4);
 
     for(x=0,c=0;c<total;x++) {
-        await fetch("https://api.giphy.com/v1/gifs/trending?api_key=HAH9qg4gGd6m3JwsSJUWkAL6mvkcEVBp&offset=" + x * 26).then(response => {return response.json();})
-        .then(json => {
+        await fetch("https://api.giphy.com/v1/gifs/trending?api_key=HAH9qg4gGd6m3JwsSJUWkAL6mvkcEVBp&offset=" + x * 26).then(function (response) {return response.json();})
+        .then(function (json) {
             var grandes = 4; // Con respecto a las chicas
             var chicas = 0;
             for(i=0;i<json.data.length;i++){
                 //console.log(json.data);
-                var node = document.createElement("li")
+                var node = document.createElement("li");
                 node.innerHTML = '<img src="'+ json.data[i].images.downsized.url +'" alt="gif"><p>#'+ json.data[i].title.split("GIF")[0] +'</p>';
+                node.setAttribute("onclick","location.href='"+json.data[i].url+"'");
                 if (json.data[i].title.split("GIF")[0] == "") {
                     node.innerHTML = '<img src="'+ json.data[i].images.downsized.url +'" alt="gif"><p>#no-title '+ json.data[i].title +'</p>';
                 }
@@ -106,5 +143,83 @@ async function trend() {
     }
 }
 
+// Search
+
+if(!document.location.pathname.includes('misGifos.html')){
+    document.getElementById("barBuscar").addEventListener("input", function() {
+        var termino = document.getElementById("barBuscar").value;
+        //console.log(termino)
+        //console.log(document.getElementsByClassName("autofill")[0].children)
+        var array = localStorage.getItem('lastSearchs').split(';');
+        for(i=array.length-1;i>=0;i--){
+            if (array[i].includes(termino) == false){
+                array.splice(i, 1);
+            }
+        }
+        //console.log(array)
+
+        array.forEach(function (valor,i) {if(i<3){
+            document.getElementsByClassName("autofill")[0].children[i].innerText = valor
+            document.getElementsByClassName("autofill")[0].children[i].setAttribute("onclick","search('"+valor+"')")
+        }});
+        
+        if(array.length<3){
+            sugerencias.forEach(function (valor,i) {
+                if(i+array.length<3){
+                    document.getElementsByClassName("autofill")[0].children[i+array.length].innerText = valor
+                    document.getElementsByClassName("autofill")[0].children[i+array.length].setAttribute("onclick","search('"+valor+"')")
+                }
+            });
+        }
+        if(termino.length >= 3) {abrirMenu('autofill',true)}
+        else {abrirMenu('autofill',false);}
+    })
+}
+function search(texto) {
+    if(texto.length >= 1) {
+        //if(localStorage.getItem('lastSearchs') == null) {localStorage.setItem('lastSearchs','')}
+        // temp = localStorage.getItem('lastSearch')
+        
+        if(!fetchSug().includes(texto)){
+            localStorage.setItem('lastSearchs', texto + ';' + localStorage.getItem('lastSearchs'))
+        }
+
+        fetch("https://api.giphy.com/v1/gifs/search?api_key=HAH9qg4gGd6m3JwsSJUWkAL6mvkcEVBp&limit=50&q="+ texto)
+            .then(function (response) {return response.json();})
+            .then(function (json) {
+                for(i=0;i<json.data.length;i++) {
+                    var node = document.createElement("li");
+                    node.innerHTML = '<img src="'+ json.data[i].images.downsized.url +'" alt="gif"><p>#'+ json.data[i].title.split("GIF")[0] +'</p>';
+                    node.setAttribute("onclick","location.href='"+json.data[i].url+"'");
+                    document.getElementById("buscadas").insertBefore(node,document.getElementById("buscadas").firstElementChild);
+                }
+                return document.getElementsByClassName('busqueda')[2].hasAttribute('hidden');
+            })
+            .then(function (buscar){
+                if(buscar) {
+                    document.getElementsByClassName('busqueda')[0].setAttribute('hidden',true);
+                    document.getElementsByClassName('busqueda')[1].setAttribute('hidden',true);
+                    abrirMenu('autofill',false);
+                    document.getElementsByClassName('busqueda')[2].removeAttribute('hidden');
+                }
+            })
+    } else {
+        if(!document.getElementsByClassName('busqueda')[2].hasAttribute('hidden')) {
+            document.getElementsByClassName('busqueda')[0].removeAttribute('hidden');
+            document.getElementsByClassName('busqueda')[1].removeAttribute('hidden');
+            document.getElementsByClassName('busqueda')[2].setAttribute('hidden',true);
+        }
+    }
+}
 
 // api_key="HAH9qg4gGd6m3JwsSJUWkAL6mvkcEVBp"
+
+// misGifos
+
+if(document.location.pathname.includes('misGifos.html')){
+    function modo() {
+        var params = new URLSearchParams(location.search)
+        if(params.get('modo') == 'crear'){return true}
+        else {return false}
+    }
+}
