ed951a2bf9be6dcc4eb5be3c4760c60cc1013021
Ezequiel Holzweissig <ezequielgh1999@gmail.com>
2020-04-11 05:59:04 -0300

    Docs mejores, Error Handling y Algunos Endpoints
    Los docs se mejoraron, ahora son funcionales para localhost y para mi dirección privada para testeo.
    
    Además, pude hacer uso de los endpoints de para ver los pedidos, y de agregar un usuario.
    
    En agregar un usuario me tiraba error y cerraba todo el servidor, así que hice un workaround para que tire el error y el servidor no se cierre. Throw termina los procesos, así que conviene usarlos solo en situaciones indispensables. (Creando la base de datos, o sus diferentes tablas.)
    
    Aparte, mejoré como se generan las tablas, utilizando serialize y parallelize.

--- tercerProyecto/server.js
+++ tercerProyecto/server.js
@@ -1,58 +1,136 @@
 const express = require('express');
-const mysql = require('sqlite3').verbose();
+const mysql = require('sqlite3')//.verbose();
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+
+// Token
+
+const firmaJWT = "ItsAMeBearer"
+
+function autenticar(username, password) {
+    db.get('SELECT Username username, Password password FROM usuarios WHERE Username = ?',username, function(err,row){
+        if(err) {
+            res.status(404).send(err)
+            console.log(err)
+            return
+        }
+        if(row.username === username.toLowerCase() && row.password === password){
+            const token = jwt.sign({"username":row.username,"id":row.id},firmaJWT)
+            return token
+        } else {
+            return false
+        }
+    })
+}
+
+function verificarToken(token) {
+    try {
+        const verify = jwt.verify(token,firmaJWT)
+        if(verify) {
+            return verify
+        }
+    } catch(err) {
+        return false
+    }
+}
 
 // Database
 
-let db = new mysql.Database(':memory:', function(err) {
+const db = new mysql.Database(':memory:', function(err) {
     if(err) throw err;
     console.log("Database creada!")
-    start()
 });
 
-function start() {
-    let sql = 'CREATE TABLE usuarios (id INT AUTO_INCREMENT PRIMARY KEY'
-    sql = sql+', username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL'
-    sql = sql+', nombre VARCHAR(255) NOT NULL, correo VARCHAR(255) NOT NULL UNIQUE'
-    sql = sql+', telefono INT UNSIGNED NOT NULL, dir VARCHAR(255) NOT NULL'
-    db.run(sql+')',function(err) {
-        if(err) throw err;
-        console.log("Creada Usuarios!")
-        admin()
+db.parallelize(function() {
+    db.serialize(function(){
+        let sql = 'CREATE TABLE usuarios (id INTEGER PRIMARY KEY'
+        sql = sql+', username VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL'
+        sql = sql+', nombre VARCHAR(255) NOT NULL, correo VARCHAR(255) NOT NULL UNIQUE'
+        sql = sql+', telefono INT UNSIGNED NOT NULL UNIQUE, dir VARCHAR(255) NOT NULL'
+        db.run(sql+')',function(err) {
+            if(err) throw err;
+            console.log("Creada Usuarios!")
+        })
+        db.run("INSERT INTO usuarios(id, username, password, nombre, correo, telefono, dir) VALUES ('0','admin','admin','admin','admin','0','admin')",function(err) {
+            if(err) throw err
+            console.log("Hecho admin")
+        })
     })
 
-    sql = 'CREATE TABLE productos (id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(255) NOT NULL'
-    sql = sql+', short VARCHAR(255) NOT NULL UNIQUE, img VARCHAR(255), precio INT UNSIGNED NOT NULL'
-    db.run(sql+')',function(err) {
-        if(err) throw err;
-        console.log("Creada Productos!")
+    db.serialize(function(){
+        sql = 'CREATE TABLE productos (id INTEGER PRIMARY KEY, nombre VARCHAR(255) NOT NULL'
+        sql = sql+', short VARCHAR(255) NOT NULL UNIQUE, img VARCHAR(255), precio INT UNSIGNED NOT NULL'
+        db.run(sql+')',function(err) {
+            if(err) throw err;
+            console.log("Creada Productos!")
+        })
     })
 
-    sql = 'CREATE TABLE pedidos (id INT AUTO_INCREMENT PRIMARY KEY, status INT UNSIGNED NOT NULL'
-    sql = sql+', hora INT UNSIGNED NOT NULL, prod VARCHAR(255) NOT NULL' // prod van a ser las ids
-    sql = sql+', usuario INT UNSIGNED NOT NULL, pago INT UNSIGNED NOT NULL' // 0 o 1 para efectivo
-    db.run(sql+')',function(err) {
-        if(err) throw err;
-        console.log("Creada Pedidos!")
+    db.serialize(function(){
+        sql = 'CREATE TABLE pedidos (id INTEGER PRIMARY KEY, status INT UNSIGNED NOT NULL'
+        sql = sql+', hora VARCHAR(255) NOT NULL, prod VARCHAR(255) NOT NULL' // prod van a ser las ids
+        sql = sql+', usuario INT UNSIGNED NOT NULL, pago INT UNSIGNED NOT NULL' // 0 o 1 para efectivo
+        db.run(sql+')',function(err) {
+            if(err) throw err;
+            console.log("Creada Pedidos!")
+        })
     })
-}
-
-function admin() {
-    db.run("INSERT INTO usuarios(username, password, nombre, correo, telefono, dir) VALUES ('admin','admin','admin','admin','0','admin')",function(err) {
-        if(err) throw err
-        console.log("Hecho admin")
-        // db.each('SELECT Nombre nombre FROM usuarios',function(err,row) {
-        //     if(err) throw err
-        //     console.log("Encontrado " + row.nombre)
-        // })
-    })
-}
+})
 
 // Express
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.get('/pedidos', function(request,response) {
-    console.log("Llegó pedido")
+// // Pedidos
+
+app.get('/pedidos\*', function(req,res) {
+    console.log("Llegó pedido API "+req.originalUrl.split('/')[2])
+    if(req.originalUrl.split('/')[2]!=undefined){
+        console.log("Hola")
+        db.get('SELECT * FROM pedidos WHERE id = ?',req.originalUrl.split('/')[2],function(err,row) {
+            if(err) {
+                res.status(400).send(err)
+                console.log(err)
+                return
+            }
+            res.status(200).json(row)
+        })
+    } else {
+        db.all('SELECT * FROM pedidos', function(err, rows) {
+            if(err) {
+                res.status(404).send(err)
+                console.log(err)
+                return
+            }
+            res.status(200).json(rows)
+        })
+    }
+})
+
+// app.post()
+// app.put()
+
+// // Productos
+
+// app.get()
+// app.post()
+// app.put()
+// app.delete()
+
+// // Usuarios
+
+app.post('/usuarios', function(req,res) {
+    const newUser = [req.body.username,req.body.password,req.body.nombre,req.body.correo,req.body.telefono,req.body.dir]
+    db.run('INSERT INTO usuarios(username, password, nombre, correo, telefono, dir) VALUES (?,?,?,?,?,?)',newUser,function(err) {
+        if(err) {
+            res.status(400).send(err)
+            console.log(err)
+            return
+        }
+        res.sendStatus(200)
+    })
 })
 
 const listener = app.listen(8000, function() {
